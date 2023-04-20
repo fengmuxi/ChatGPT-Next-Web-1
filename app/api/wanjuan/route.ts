d8bd515ff8f188e996c5c2cde27b0c9f184e90fb
xiao <1809489542@qq.com>
2023-04-20 10:31:15 +0800

    测试

--- app/api/wanjuan/route.ts
+++ app/api/wanjuan/route.ts
@@ -1,22 +1,20 @@
 export async function POST(req: Request) {
   try {
     let token = process.env.WANJUAN_TOKEN;
-    // let body = { message: await req.json() };
-    let body = {"msgContent": await req.json(), "chatID": "104000676614877184"};
+    let body = { message: await req.json() };
 
     console.log(JSON.stringify(body));
     let res = "";
-    await fetch("https://insi.chat/v1/api/chat/msg?app_id=1009", {
+    await fetch("http://47.94.237.159:8080/v1/wanjuan", {
       method: "POST",
       headers:{
-        // "Authorization":"Bearer "+token
-        "token": token
+        "Authorization":"Bearer "+token
       },
       body: JSON.stringify(body),
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+        // console.log(data)
         if (data["statusInfo"]["code"] == 0) {
           // console.log("123123")
           res = data["data"]["msgContent"];
