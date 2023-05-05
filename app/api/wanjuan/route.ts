7e3c88c103bfbe720e6d56cfc2399bcd570257b8
fengmuxi <90232379+fengmuxi@users.noreply.github.com>
2023-05-05 09:22:02 +0000

    万卷修改

--- app/api/wanjuan/route.ts
+++ app/api/wanjuan/route.ts
@@ -1,18 +1,15 @@
-import { NextRequest } from "next/server";
-import { auth } from "../auth";
-
-export async function POST(req: NextRequest) {
-  const authResult = auth(req);
-  if (authResult.error) {
-    return authResult.msg;
-  }
+export async function POST(req: Request) {
   try {
+    let token = process.env.WANJUAN_TOKEN;
     let body = { message: await req.json() };
 
     console.log(JSON.stringify(body));
     let res = "";
     await fetch("http://47.94.237.159:8080/v1/wanjuan", {
       method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
       body: JSON.stringify(body),
     })
       .then((response) => response.json())
